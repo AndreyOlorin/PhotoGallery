23649288e1d0f65b3f9da4871bee613ad3ae72ac
AndreyOlorin <sap7andy@gmail.com>
2024-12-16 21:48:14 +0700

    New

--- app/src/main/java/com/cyberbyte/photogallery/ui/view/MainActivity.kt
+++ app/src/main/java/com/cyberbyte/photogallery/ui/view/MainActivity.kt
@@ -39,5 +39,5 @@ class MainActivity : AppCompatActivity() {
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
-    }//3
+    }//4
 }
